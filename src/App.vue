e9f1647444e6a118b66726b1b0f765613843bcc0
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2020-01-18 07:27:32 -0300

    vai teia

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <input type="text" v-model="text" @keyup.enter="encode">
+    <audio v-if="show" controls>
+      <source :src="encoded" type="audio/ogg">
+      Your browser does not support the audio element.
+    </audio>
+  </div>
+</template>
+
+<script>
+import { Base64 } from 'js-base64'
+export default {
+  name: 'app',
+  components: {},
+  watch: {
+    encoded (value) {
+      if (!value) {
+        this.show = false
+      }
+    }
+  },
+  data: () => ({
+    text: '',
+    encoded: '',
+    show: false
+  }),
+  methods: {
+    encode () {
+      if (this.show) {
+        this.show = false
+      }
+      // eslint-disable-next-line no-debugger
+      debugger
+      this.encoded = `https://voice2.reverso.net/RestPronunciation.svc/v1/output=json/GetVoiceStream/voiceName=Heather22k?inputText=${Base64.encode(this.text)}`
+      this.show = true
+    }
+  },
+  created () {
+    console.log(Base64.encode('bielzinho')
+    )
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
