d2367866875b74c3ecec437e72b4fd7b19c4f75b
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2020-03-04 21:21:56 -0400

    vai teia

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
   <div id="app">
-    <input ref="inputkk" type="text" v-model="text" @keyup.enter="encode">
-    <audio v-if="show" controls>
-      <source :src="encoded" type="audio/mp3">
-      Your browser does not support the audio element.
-    </audio>
+    <div>
+      <input ref="inputkk" type="text" v-model="text" @keyup.enter="encode">
+    </div>
+    <div>
+      <audio v-if="show" controls>
+        <source :src="encoded" type="audio/mp3">
+        Your browser does not support the audio element.
+      </audio>
+    </div>
+    <div>
+      <textarea id="story" name="story"
+        v-model="textArea" rows="5" cols="33" />
+    </div>
   </div>
 </template>
 
@@ -23,15 +31,20 @@ export default {
   data: () => ({
     text: '',
     encoded: '',
+    textArea: '',
     show: false
   }),
   methods: {
     encode () {
+      this.textArea = ''
       this.$refs['inputkk'].select()
       document.execCommand('copy')
       // eslint-disable-next-line no-debugger
-      debugger
       this.show = !this.show
+      let createTitle = this.text.split(' ')
+      createTitle.forEach((value) => {
+        this.textArea += `<span title="">${value}</span> \n\n`
+      })
       this.encoded = `https://voice2.reverso.net/RestPronunciation.svc/v1/output=json/GetVoiceStream/voiceName=Heather22k?inputText=${Base64.encode(this.text)}&export=download`
     }
   }
